a9ba14575ef42186dd6f1cf29add646851ac5a6f
Fromtdnf <fromtdnf@gmail.com>
2025-05-26 16:41:52 -0300

    server.js

--- server.js
+++ server.js
@@ -15,11 +15,6 @@ app.get('/', (req, res) => {
   res.send('API do Cassino Online está funcionando!');
 });
 
-const express = require('express');
-const app = express();
-
-// Sua lógica e rotas aqui...
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
